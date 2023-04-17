9ef1c10b6721c09bdd17d93bfe83d9eb65ee15b6
MrchllAnuu.My.Id <95945703+MrchllAnuu@users.noreply.github.com>
2023-04-18 06:47:15 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
+global.creator = 'Mrchlldev'// yourname
 global.MONGO_DB_URI = "" //database mongodb 
-global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.ACTIVATION_TOKEN_SECRET = "abcdefghjiklmnopqrstuvwxyzABCDEVGHIJKLMNOPQRSTUVWXYZ1234567890" //isi apa aja bebas
+global.your_email = "cs@mrchll.online" //email
+global.email_password = "Mrchlldev@123" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
